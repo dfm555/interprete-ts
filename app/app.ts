fc109ee8b087fac29d1271adcbbc64b30f9f7c4b
dfm555 <dfm556@gmail.com>
2016-09-13 00:34:36 -0500

    base interprete implementado

--- app/app.ts
+++ app/app.ts
@@ -4,4 +4,24 @@
 import {Lexer} from './interprete/Lexer'
 import {Token} from './interprete/Token'
 
+class App{
+    public static main():void {
+        let programa: string = "2+ 233 +  4.5";
+        let lexer:Lexer = new Lexer(programa);
+        while(!lexer.match(Token.FIN_ARCHIVO) ){
+            if( lexer.match(Token.VALOR_ENTERO) ){
+                document.write("Entero " + lexer.obtenerEntero()+"<br>");
+            }
+            if( lexer.match(Token.SUMA) ){
+                document.write("SUMA "+"<br>");
+            }
 
+            if( lexer.match(Token.VALOR_REAL) ){
+                document.write("Real "+ lexer.obtenerReal()+"<br>");
+            }
+            lexer.advance();
+        }
+    }
+}
+
+App.main()
--- app/interprete/Token.ts
+++ app/interprete/Token.ts
@@ -2,6 +2,37 @@
  * Created by duber on 12/09/16.
  */
 class Token {
+    public static FIN_ARCHIVO: number = -1;
+    //
+    public static ERROR:number = -2;
+    //
+    public static  SUMA:number = 1;
+    public static  RESTA:number = 2;
+    public static  MULTIPLICACION:number = 3;
+    public static  DIVISION:number = 4;
+    public static  POTENCIA:number = 5;
+    public static  MODULO:number = 6;
+    public static  DIVISION_ENTERA:number = 7;
+
+    public static  ABRIR_PARENTESIS:number = 8;
+    public static  CERRAR_PARENTESIS:number = 9;
+    public static  PI:number = 10;
+
+    //
+    public static  MENOR_QUE:number = 11;
+    public static  MAYOR_QUE:number = 12;
+    public static  IGUAL_QUE:number = 13;
+    public static  MENOR_IGUAL_QUE:number = 14;
+    public static  MAYOR_IGUAL_QUE:number = 15;
+    public static  DIFERENTE:number = 16;
+    //
+    public static  Y_LOGICO:number = 17;
+    public static  O_LOGICO:number = 18;
+    public static  NO_LOGICO:number = 19;
+
+    //
+    public static  VALOR_ENTERO:number = 100;
+    public static  VALOR_REAL:number = 101;
 
 }
 
